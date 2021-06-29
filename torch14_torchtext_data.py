75447bedc7ccb4872951817c8292b0ee5071a7ad
HYUNMIN-HWANG <hwangkei0212@gmail.com>
2021-06-29 16:46:51 +0900

    21-06-29

---
+++ torch14_torchtext_data.py
@@ -0,0 +1,18 @@
+# IMDb Dataset
+# 간단한 Sentiment Analysis Task
+
+from typing import Sequence
+from torchtext.legacy import data
+from torchtext.legacy import datasets
+
+# Data Setting
+TEXT = data.Field(lower=True, batch_first=True)
+# lower : 문장을 모두 소문자화하는 옵션
+# batch_first : batch_size 를 가장 앞으로 설정하는 옵션
+LABEL = data.Field(sequential=False)
+
+train, test = datasets.IMDB.splits(TEXT, LABEL)
+# splits : IMDB에 있는 데이터를 train, test 데이터셋으로 쉽게 가져올 수 있다.
+
+print(train)    # <torchtext.legacy.datasets.imdb.IMDB object at 0x0000024E1482A828>
+print(test)     # <torchtext.legacy.datasets.imdb.IMDB object at 0x0000024E1482AFD0>
